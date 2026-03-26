1aaf13c4af8f3432f42f2ef4f329105eaf93131c
Kengwong <Kengkeka@gmail.com>
2026-03-26 21:13:19 +0700

    Clean up tracked files and apply gitignore

--- run_auto_content.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Auto Content Generator Starting...
-cd /d C:\Users\Administrator
-python "D:\Projects\krobjang-site full\auto_content_krobjang.py" >> "D:\Projects\krobjang-site full\auto_content_log.txt" 2>&1
-echo Done at %date% %time%
